25a0edeacbbfdd81b73caf70abd0c2d636573fac
yycx <1975427683@qq.com>
2017-08-08 15:53:03 +0800

    this is node js

---
+++ http.js
@@ -0,0 +1,10 @@
+var http = require('http')
+
+http
+	.createServer(function(req,res){
+		res.writeHead(200,{'Content-Type':'text/plain'})
+		res.write('hello nodejs')
+		res.end()
+	})
+	.listen(2015)
+	console.log('Welcome to you!')
